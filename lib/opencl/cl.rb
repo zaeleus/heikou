0e90963e9747bcad23bf86741e06bc88f604052c
Michael Macias <mamacias@olemiss.edu>
2012-11-18 01:12:50 -0600

    Move device listing to its respective class

--- lib/opencl/cl.rb
+++ lib/opencl/cl.rb
@@ -15,18 +15,7 @@ module OpenCL
     end
 
     def devices
-      @devices ||= begin
-        platform = default_platform
-
-        num_devices = FFI::MemoryPointer.new :uint
-        clGetDeviceIDs(platform.id, CL_DEVICE_TYPE_ALL, 0, nil, num_devices)
-
-        n = num_devices.read_uint
-        devices = FFI::MemoryPointer.new :pointer, n
-        clGetDeviceIDs(platform.id, CL_DEVICE_TYPE_ALL, n, devices, nil)
-
-        devices.read_array_of_pointer(n).map { |ptr| Device.new(self, ptr) }
-      end
+      @devices ||= Device.all(cl)
     end
 
     def default_device
--- lib/opencl/device.rb
+++ lib/opencl/device.rb
@@ -4,6 +4,21 @@ module OpenCL
 
     attr_reader :id
 
+    def self.all(cl)
+      platform = cl.default_platform
+
+      num_devices = FFI::MemoryPointer.new FFI::OpenCL.find_type(:cl_uint)
+      clGetDeviceIDs(platform.id, CL_DEVICE_TYPE_ALL, 0, nil, num_devices)
+
+      n = num_devices.read_uint
+      devices = FFI::MemoryPointer.new :pointer, n
+      clGetDeviceIDs(platform.id, CL_DEVICE_TYPE_ALL, n, devices, nil)
+
+      devices.read_array_of_pointer(n).map do |ptr|
+        Device.new(self, ptr)
+      end
+    end
+
     def initialize(cl, id)
       @cl = cl
       @id = id
